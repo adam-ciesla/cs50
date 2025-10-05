7de6d4567bcbe5e047f67df0d7f0ff66bd1d9c07
Adam Cieśla <aciesla@proton.me>
2025-10-05 15:51:25 +0200

    Update helpers.c

--- pset_4/filter-less/helpers.c
+++ pset_4/filter-less/helpers.c
@@ -15,7 +15,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             int blueValue = image[i][j].rgbtBlue;
 
             // Calculate the average pixel value for grayscale effect
-            int averageValue = (int)round((redValue + greenValue + blueValue) / 3.0);
+            int averageValue = (int) round((redValue + greenValue + blueValue) / 3.0);
 
             // Set each colour value to the average
             image[i][j].rgbtRed = averageValue;
@@ -39,9 +39,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int blueValue = image[i][j].rgbtBlue;
 
             // Calculate the sepia value for each colour
-            int sepiaRed = (int)round(.393 * redValue + .769 * greenValue + .189 * blueValue);
-            int sepiaGreen = (int)round(.349 * redValue + .686 * greenValue + .168 * blueValue);
-            int sepiaBlue = (int)round(.272 * redValue + .534 * greenValue + .131 * blueValue);
+            int sepiaRed = (int) round(.393 * redValue + .769 * greenValue + .189 * blueValue);
+            int sepiaGreen = (int) round(.349 * redValue + .686 * greenValue + .168 * blueValue);
+            int sepiaBlue = (int) round(.272 * redValue + .534 * greenValue + .131 * blueValue);
 
             // If any of the sepia values exceeds the max of 255, set it to 255
             if (sepiaRed > 255)
@@ -76,8 +76,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         {
             // Change values of pixels on the opposite sites of the row
             RGBTRIPLE temp = image[i][j];
-            image[i][j] = image[i][width - j];
-            image[i][width - j] = temp;
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = temp;
         }
     }
 }
@@ -85,5 +85,50 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Create a copy of the original image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    // Iterate through each row
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate through each pixel in that row
+        for (int j = 0; j < height; j++)
+        {
+            float redSum = 0.0, greenSum = 0.0,
+                  blueSum = 0.0; // Sum of all RGB values of surrounding pixels
+            int counter = 0;     // Count of pixels that are important
+
+            // Iterate over the (3x3) neighbourhood around (i, j) including (i,j)
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ni = i + di;
+                    int nj = j + dj;
+
+                    // Checking if we haven't reached outside the bitmap
+                    if (ni >= 0 && ni < height && nj >= 0 && nj < width)
+                    {
+                        redSum += copy[ni][nj].rgbtRed; // Referring to the original image
+                        greenSum += copy[ni][nj].rgbtGreen;
+                        blueSum += copy[ni][nj].rgbtBlue;
+
+                        counter++;
+                    }
+                }
+            }
+
+            // Setting each RGB value to the average of the values in the (3x3) neighbourhood
+            image[i][j].rgbtRed = (int) round(redSum / counter);
+            image[i][j].rgbtGreen = (int) round(greenSum / counter);
+            image[i][j].rgbtBlue = (int) round(blueSum / counter);
+        }
+    }
 }
